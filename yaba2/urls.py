b50ca2c1f3a68b06ec2714f22a346d4b3e26c864
f4nt <f4nt@f4ntasmic.com>
2011-12-23 13:05:44 -0600

    Borrowing heavily from twitter's bootstrap. Getting a quick theme and view together

--- yaba2/urls.py
+++ yaba2/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('yaba2.views',
-
+    url(r'^$', 'index', name='index'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
