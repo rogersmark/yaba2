df01ea25ac85956026b83d62ab27d9a2fc9efca5
f4nt <f4nt@f4ntasmic.com>
2012-03-05 19:52:24 -0600

    more general cleanup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 try:
-    from setuptools import setup, find_packages, Command
+    from setuptools import setup, find_packages
 except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
-    from setuptools import setup, find_packages, Command
+    from setuptools import setup, find_packages
 
 
 install_requires = [
-    'django-taggit'
+    'django-taggit',
     'django-markdown',
 ]
 
